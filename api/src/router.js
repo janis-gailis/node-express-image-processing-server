8c04e3740763b2e3e6ded766c6597ad4cfa23957
janis-gailis <83454700+janis-gailis@users.noreply.github.com>
2023-02-24 10:28:04 +0200

    Completed module

--- api/src/router.js
+++ api/src/router.js
@@ -12,7 +12,7 @@ const storage = multer.diskStorage({
     filename,
 });
 
-const filefilter = (request, file, callback) => {
+const fileFilter = (request, file, callback) => {
     if (file.mimetype !== 'image/png') {
         request.fileValidationError = 'Wrong file type';
         callback(null, false, new Error('Wrong file type'))
