7f5504e1be75530f78e52192221358104c44531e
janis-gailis <83454700+janis-gailis@users.noreply.github.com>
2023-02-24 10:11:00 +0200

    Completed module

--- api/src/router.js
+++ api/src/router.js
@@ -1,5 +1,35 @@
-const Router() = require('express')
-const router = Router()
-module.exports = router
+const Router() = require('express');
+const multer = require('multer');
 
-function filename('request', file, callback)
+const router = Router();
+
+const filename = (request, file, callback) => {
+    callback(null, file.originalname)
+};
+
+const storage = multer.diskStorage({
+    destination = 'api/uploads',
+    filename,
+});
+
+const filefilter = (request, file, callback) => {
+    if (file.mimetype !== 'image/png') {
+        request.fileValidationError = 'Wrong file type';
+        callback(null, false, new Error('Wrong file type'))
+    } else {
+        callback(null, true);
+    }
+};
+const upload = multer({
+    fileFilter,
+    storage,
+})
+
+router.post('/upload', upload.single('photo'), (request, response) => {
+    if (request.fileValidationError) return response.status(400).json({ error: request.fileValidationError })
+
+    return response.status(201).json({ success: true });
+});
+
+
+module.exports = router;
